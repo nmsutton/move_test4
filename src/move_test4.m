c59c6c41745b06ba9c025ac27773e88509fc849e
Nate Sutton <natemsutton@gmail.com>
2022-04-28 17:46:02 -0400

    bumps formed with 0.8 U

--- src/move_test4.m
+++ src/move_test4.m
@@ -2,7 +2,7 @@
 % Nate Sutton 2022
 clear all;
 clc;
-simdur = 810;%3010;%210;%130;%1010;%490;%1300;%100e3; % total simulation time, ms
+simdur = 530;%3010;%210;%130;%1010;%490;%1300;%100e3; % total simulation time, ms
 spiking_bin = 40;%40;
 
 ncells = 900; % total number of cells per layer
@@ -42,20 +42,20 @@ in_firings = in_firings_init;
 load('../../move_test3/data/B_saved.mat'); % velocity input matrix
 %ext_ie=68*(B.^20)'; % excitatory input
 ext_ie=60*(B.^22)'; % excitatory input
-pd_match=88;%67;%132;%58;%%115;%89;%88;%74;%71.5;%83;%83;%60;%51;%42;%34.4;%43;
+pd_match=67;%132;%58;%%115;%89;%88;%74;%71.5;%83;%83;%60;%51;%42;%34.4;%43;
 pd_nonmatch=60;%90;%30;%80;%60;
 load('../../move_test3/data/mex_hat3.mat'); % load weight matrix
 mex_hat = mex_hat3*3;
 mex_hat = mex_hat-0.0022;
 mex_hat = mex_hat.*(mex_hat>0); % no negative values
-gc_to_in_wt = 180;%180;%30;%39;%180;%0.4;%0.2;%0.121;%;//0.12;%0.15; % gc to in synapse weight
+gc_to_in_wt = 47;%100;%180;%180;%30;%39;%180;%0.4;%0.2;%0.121;%;//0.12;%0.15; % gc to in synapse weight
 in_to_gc_wt = .45;%.45;%.39;%.15;%.15;%.3;%.15; % in to gc synapse weight
 
 % tm model synapse parameters
 global cap_u tau_u tau_x tau_d g u_ei x_ei u_ie x_ie;
-cap_u = 1;%0.2; % U, utilization
+cap_u = .8;%9;%0.2; % U, utilization
 tau_u = 30;%40.0; % U signal decay time constant
-tau_x = 30;%100.0; % x signal decay time constant
+tau_x = 15;%30;%100.0; % x signal decay time constant
 tau_d = 30.0; % x signal decay time constant
 g = 1.0;
 u_ei = zeros(ncells,1); % u before spike update
