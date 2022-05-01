965159d3bd87489503596b08314075d498087f4f
Nate Sutton <natemsutton@gmail.com>
2022-05-01 12:18:23 -0400

    bumps forming with CARLsim real U values

--- src/move_test4.m
+++ src/move_test4.m
@@ -2,7 +2,7 @@
 % Nate Sutton 2022
 clear all;
 clc;
-simdur = 2010;%530;%170;%530;%3010;%210;%130;%1010;%490;%1300;%100e3; % total simulation time, ms
+simdur = 4010;%530;%170;%530;%3010;%210;%130;%1010;%490;%1300;%100e3; % total simulation time, ms
 spiking_bin = 40;%40;
 
 ncells = 900; % total number of cells per layer
@@ -53,16 +53,16 @@ mex_hat = mex_hat3*3;%3;
 mex_hat = mex_hat-0.0022;
 mex_hat = mex_hat.*(mex_hat>0); % no negative values
 mult_in = 230;%250;%330;%27;
-gc_to_in_wt = 25*mult_in;%25;%25;%25;%180;%25;%36;%47;%100;%180;%180;%30;%39;%180;%0.4;%0.2;%0.121;%;//0.12;%0.15; % gc to in synapse weight
-in_to_gc_wt = 20*mult_in;%50;%60;%70;%60;%50;%70;%410;%1200;%410;%410;%.45;%.45;%.39;%.15;%.15;%.3;%.15; % in to gc synapse weight
+gc_to_in_wt = mult_in*15;%15;%25;%25;%25;%180;%25;%36;%47;%100;%180;%180;%30;%39;%180;%0.4;%0.2;%0.121;%;//0.12;%0.15; % gc to in synapse weight
+in_to_gc_wt = mult_in*20;%50;%60;%70;%60;%50;%70;%410;%1200;%410;%410;%.45;%.45;%.39;%.15;%.15;%.3;%.15; % in to gc synapse weight
 
 % tm model synapse parameters
 global cap_ue tau_ue tau_xe tau_de gei u_ei x_ei ...
 	   cap_ui tau_ui tau_xi tau_di gie u_ie x_ie;
-cap_ue = .3;%.5;%.6;%.5;%.6;%.7;%.8;%9;%0.2; % U, utilization
+cap_ue = 0.1638;%.2;%0.1638;%.3;%.5;%.6;%.5;%.6;%.7;%.8;%9;%0.2; % U, utilization
 tau_ue = 15;%40.0; % U signal decay time constant
 tau_xe = 7.5;%15;%30;%100.0; % x signal decay time constant
-tau_de = 30.0; % x signal decay time constant
+tau_de = 45;%30.0; % x signal decay time constant
 gei = 1.0;
 cap_ui = 0.1119;%1;%.8;%1;%.4;%.5;%.6;%.8;%1;%.8;%9;%0.2; % U, utilization
 tau_ui = 60;%70;%90;%60;%50;%50;%30;%40.0; % U signal decay time constant; facilitations factor?
